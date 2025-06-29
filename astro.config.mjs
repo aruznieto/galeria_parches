bf889a628023a8cc5ac8e801c9bcc7e7a4e314eb
Andrés Ruz Nieto - MAC <40019177+aruznieto@users.noreply.github.com>
2025-06-29 12:38:01 +0200

    feat: add GitHub Pages deployment workflow and configure site settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 import image from '@astrojs/image';
 
 export default defineConfig({
+  site: 'https://parches.anrn.dev',
+  base: "galeria_parches",
   integrations: [image()],
   vite: {
     plugins: [tailwindcss()],
